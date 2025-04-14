84c3048da49d1ba6b4fcfa8837ede7f3d94edca9
noname27cloud <akmonshak.27i@gmail.com>
2025-04-14 21:57:15 +0500

    cin

--- js/app.js
+++ js/app.js
@@ -93,26 +93,40 @@ function generateArticles() {
       const shuffled = data.sort(() => 0.5 - Math.random());
       const randomArticles = shuffled.slice(0, 3);
 
-      randomArticles.forEach((article, index) => {
+      randomArticles.forEach((article) => {
         const articleDiv = document.createElement("div");
         articleDiv.classList.add("slider-article");
-        if (index === 0) articleDiv.classList.add("active");
 
         const duration = article.meta.duration_days;
-        const technologies = article.meta.technologies.join(", ");
+        const techList = article.meta.technologies || [];
+        const worksList = article.meta.works || [];
+        const fullList = [...techList, ...worksList];
+        const formattedMeta = fullList.join(" | ");
 
         articleDiv.innerHTML = `
+        <div class="projects-article">
           <div class="article-image"> 
             <img src="${article.articleImage}" alt="Article Image" class="article-img"/>
           </div>
           <div class="article-info">
             <p class="article-tag">${article.tag}</p>
-            <h3 class="article-title">${article.title}</h3>
+            <h3 class="article-title">
+  <a href="#" class="article-link" >
+    ${article.title}
+    <svg class="article-icon" width="24" height="24" viewBox="0 0 24 24" fill="none"
+         xmlns="http://www.w3.org/2000/svg">
+      <path d="M7 17L17 7M17 7H7M17 7V17" stroke="currentColor" stroke-width="2"
+            stroke-linecap="round" stroke-linejoin="round"/>
+    </svg>
+  </a>
+</h3>
+
             <p class="article-description">${article.description}</p>
           </div>
           <div class="article-data">
-            <p>${duration} days | ${technologies}</p>
-            <p>${article.created_at}</p>
+            <p class="article-duration">${duration} days | ${formattedMeta}</p>
+            <p class="article-created">${article.created_at}</p>
+          </div>
           </div>
         `;
 
