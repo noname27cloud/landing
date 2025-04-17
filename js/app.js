bc15915b28c0ead8154860d642704a08b24c1974
noname27cloud <akmonshak.27i@gmail.com>
2025-04-18 02:00:27 +0500

    cin

--- js/app.js
+++ js/app.js
@@ -213,31 +213,56 @@ function faq() {
     });
 }
 
-// Get the button:
-let mybutton = document.getElementById("myBtn");
-
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function () {
-  scrollFunction();
-};
-
-function scrollFunction() {
-  if (
-    document.body.scrollTop > 300 ||
-    document.documentElement.scrollTop > 300
-  ) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
-
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
+  // Get the button:
+  let mybutton = document.getElementById("myBtn");
+
+  // When the user scrolls down 20px from the top of the document, show the button
+  window.onscroll = function () {
+    scrollFunction();
+  };
+
+  function scrollFunction() {
+    if (
+      document.body.scrollTop > 300 ||
+      document.documentElement.scrollTop > 300
+    ) {
+      mybutton.style.display = "block";
+    } else {
+      mybutton.style.display = "none";
+    }
+  }
+
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
 
+function themeToggle() {
+  const themeCheckbox = document.querySelector("#theme-toggle");
+  const pageTheme = document.body;
+  let currentTheme = localStorage.getItem("theme");
+
+  if (!currentTheme) {
+    currentTheme = "light";
+    localStorage.setItem("theme", currentTheme);
+  }
+
+  if (currentTheme === "dark") {
+    pageTheme.classList.add("dark-theme");
+    themeCheckbox.checked = true;
+  }
+
+  themeCheckbox.addEventListener("change", () => {
+    pageTheme.classList.toggle("dark-theme");
+    const theme = pageTheme.classList.contains("dark-theme") ? "dark" : "light";
+    localStorage.setItem("theme", theme);
+  });
+}
+
+themeToggle();
+
+topFunction();
 faq();
 generateArticles();
 generateSlider();
